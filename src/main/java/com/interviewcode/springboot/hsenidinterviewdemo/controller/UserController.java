ef4540ea213d4f1514eb041cb890932bbb4a2753
ravidugm <ravindumiyuranga@gmail.com>
2021-04-11 10:55:07 +0530

    Add REST controller for save a new Admin

--- src/main/java/com/interviewcode/springboot/hsenidinterviewdemo/controller/UserController.java
+++ src/main/java/com/interviewcode/springboot/hsenidinterviewdemo/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.interviewcode.springboot.hsenidinterviewdemo.controller;
 
+import com.interviewcode.springboot.hsenidinterviewdemo.entity.Role;
 import com.interviewcode.springboot.hsenidinterviewdemo.entity.User;
 import com.interviewcode.springboot.hsenidinterviewdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -38,5 +42,15 @@ public class UserController {
         theModel.addAttribute("admin", theAdmin);
 
         return "/admin-registration-form";
+    }
+
+    @PostMapping("/saveAdmin")
+    public String saveAdmin(@ModelAttribute("admin") User theAdmin) {
+
+        theAdmin.setRoles(Arrays.asList(new Role("ADMIN")));
+
+        userService.save(theAdmin);
+
+        return "redirect:/users/list";
     }
 }
