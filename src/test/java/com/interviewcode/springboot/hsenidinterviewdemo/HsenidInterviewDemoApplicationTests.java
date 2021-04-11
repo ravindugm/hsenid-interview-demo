88b26f780922e718aa302710f5a2a0780029b210
ravidugm <ravindumiyuranga@gmail.com>
2021-04-11 09:44:30 +0530

    Initial Commit

---
+++ src/test/java/com/interviewcode/springboot/hsenidinterviewdemo/HsenidInterviewDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewcode.springboot.hsenidinterviewdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsenidInterviewDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
